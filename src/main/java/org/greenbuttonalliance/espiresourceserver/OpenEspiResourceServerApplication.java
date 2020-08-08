1d9b328b6b156346f4f270c42664dbbf3284771d
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2020-08-07 23:48:36 -0400

    Initial commit

---
+++ src/main/java/org/greenbuttonalliance/espiresourceserver/OpenEspiResourceServerApplication.java
@@ -0,0 +1,13 @@
+package org.greenbuttonalliance.espiresourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenEspiResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenEspiResourceServerApplication.class, args);
+	}
+
+}
